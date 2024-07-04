9e7551365bc8a843af3f9fc817927a2b57c3757d
Jericho1611 <joshmg426@gmail.com>
2024-07-04 00:19:03 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
